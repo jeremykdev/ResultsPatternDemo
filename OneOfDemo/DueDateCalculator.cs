94a548d1fdd79b5baffbba103a83b5eaa53f31c8
jeremykdev <jeremykdev@gmail.com>
2024-12-28 12:02:36 -0700

    refactor DueDateCalculator

--- OneOfDemo/DueDateCalculator.cs
+++ OneOfDemo/DueDateCalculator.cs
@@ -21,28 +21,49 @@ public static class DueDateCalculator
         return new DateOnly(year, month, 1).AddMonths(1).AddDays(-1);
     }
 
-
-    public static DateOnly GetDueDate(OneOf<DateTime, int, DueOn> input)
+     
+    public static OneOf<DateOnly, string> GetDueDate(OneOf<DateTime, int, DueOn> input)
     {
-        return input.Match<DateOnly>(
-            dateTime => DateOnly.FromDateTime(dateTime),
-            i => DateOnly.FromDateTime(DateTime.Today.AddDays(i)),
-            dueOn =>
-            {
-                switch (dueOn)
-                {
-                    case DueOn.NinetyDaysFromToday:
-                        return DateOnly.FromDateTime(DateTime.Today.AddDays(90));
-                        break;
-                    case DueOn.FirstDayOfNextMonth:
-                        return GetFirstDayOfMonth(DateTime.Today.AddMonths(1));
-                        break;
-                    case DueOn.LastDayOfNextMonth:
-                    default:
-                        return GetLastDayOfMonth(DateTime.Today.AddMonths(1));
-                        break;
-                }
-            }
-        );
+        DateOnly? date = null;
+        string? message = null;
+        
+        input.Switch(
+             dateTime => date = DateOnly.FromDateTime(dateTime),
+             i => {
+                 if (i < 1)
+                     message = "Must provide a postive number of days";
+                 else
+                     date = DateOnly.FromDateTime(DateTime.Today.AddDays(i));
+                 },
+             dueOn =>
+             {
+                 switch (dueOn)
+                 {
+                     case DueOn.NinetyDaysFromToday:
+                         date = DateOnly.FromDateTime(DateTime.Today.AddDays(90));
+                         break;
+                     case DueOn.FirstDayOfNextMonth:
+                         date = GetFirstDayOfMonth(DateTime.Today.AddMonths(1));
+                         break;
+                     case DueOn.LastDayOfNextMonth:
+                     default:
+                         date = GetLastDayOfMonth(DateTime.Today.AddMonths(1));
+                         break;
+                 }
+             }
+         );
+
+        if (message is not null)
+            return message;
+
+        if (date.HasValue)
+        {
+            if (date.Value < DateOnly.FromDateTime(DateTime.Today.AddDays(1)))
+                return "Date must be in the future";
+            else
+                return date.Value;
+        }
+
+        return "Unable to calculate";
     }
 }
--- OneOfDemo/FrmMain.cs
+++ OneOfDemo/FrmMain.cs
@@ -1,3 +1,5 @@
+using OneOf;
+
 namespace OneOfDemo;
 
 public partial class Form1 : Form
@@ -9,33 +11,35 @@ public partial class Form1 : Form
 
     private void btnCalculate_Click(object sender, EventArgs e)
     {
-        DateOnly? date = null;
+        OneOf<DateOnly, string> result = null;
 
         if (this.rdo90Days.Checked)
         {
-            date = DueDateCalculator.GetDueDate(DueOn.NinetyDaysFromToday);
+            result = DueDateCalculator.GetDueDate(DueOn.NinetyDaysFromToday);
         }
         else if (this.rdoFirstDayOfNextMonth.Checked)
         {
-            date = DueDateCalculator.GetDueDate(DueOn.FirstDayOfNextMonth);
+            result = DueDateCalculator.GetDueDate(DueOn.FirstDayOfNextMonth);
         }
         else if (this.rdoLastDayOfNextMonth.Checked)
         {
-            date = DueDateCalculator.GetDueDate(DueOn.LastDayOfNextMonth);
+            result = DueDateCalculator.GetDueDate(DueOn.LastDayOfNextMonth);
         }
         else if (this.rdoNumberOfDaysFromToday.Checked)
         {
             if (Int32.TryParse(this.txtNumberOfDays.Text.Trim(), out var days))
-                date = DueDateCalculator.GetDueDate(days);
+                result = DueDateCalculator.GetDueDate(days);
             else
                 this.lblResult.Text = "Please enter number of days";
         }
         else if (this.rdoSpecificDate.Checked)
         {
-            date = DueDateCalculator.GetDueDate(this.dpSpecificDate.Value);
+            result = DueDateCalculator.GetDueDate(this.dpSpecificDate.Value);
         }
 
-        if (date.HasValue)
-            this.lblResult.Text = $"Due Date: {date:dd MMM yyyyy}";
+       this.lblResult.Text = result.Match(
+            date => $"Due Date: {date:dd MMM yyyyy}",
+            message => message 
+        );
     }
 }
